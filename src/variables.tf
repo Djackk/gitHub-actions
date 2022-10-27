219760ceb0bd4d246d6da143af2dd9db5e5ca830
siddharthjumani <siddharth.jumani@takeoff.com>
2022-10-27 19:03:29 +0530

    workflow_dispatch

--- src/variables.tf
+++ src/variables.tf
@@ -4,7 +4,7 @@ variable "region" {
 }
 
 variable "domain" {
-  default     = "inbound"
+  default     = "outbound"
   description = "The domain for the component e.g. Outbound, Inbound, Production etc"
 }
 
